787e003c6e3d70a262acf286d3fbf7904ea5bfe3
VladiGanchev <vladimirganchev1>
2024-03-24 14:04:29 +0200

    Added class Repository with CRUD Methods

---
+++ FootballProjectSoftUni.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballProjectSoftUni.Infrastructure.Data.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+        IQueryable<T> AllReadOnly<T>() where T : class;
+
+
+    }
+}

d813eecbda4aabe51b6cd3a2868a3cb8ce8e794f
VladiGanchev <vladimirganchev1>
2024-03-24 14:07:19 +0200

    Action City/All implemented and modified action Index

--- FootballProjectSoftUni/Controllers/CityController.cs
+++ FootballProjectSoftUni/Controllers/CityController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FootballProjectSoftUni.Core.Contracts.City;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FootballProjectSoftUni.Controllers
 {
     public class CityController : Controller
     {
-        public IActionResult Index()
+        private readonly ICityService cityService;
+
+        public CityController(ICityService _service)
         {
-            return View();
+            cityService = _service;
+        }
+
+        public async Task<IActionResult> All(int? page)
+        {
+
+            var cities = await cityService.All(page);
+            return View(cities);
         }
     }
 }

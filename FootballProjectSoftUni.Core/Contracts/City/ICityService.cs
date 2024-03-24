e704d426bcdb16e830605071bdbf568d3ddfb869
VladiGanchev <vladimirganchev1>
2024-03-24 14:05:12 +0200

    CityService class and interface added

--- FootballProjectSoftUni.Core/Contracts/City/ICityService.cs
+++ FootballProjectSoftUni.Core/Contracts/City/ICityService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FootballProjectSoftUni.Core.Models.City;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace FootballProjectSoftUni.Core.Contracts.City
 {
-    internal class ICityService
+    public interface ICityService
     {
+        Task<IEnumerable<CityViewModel>> All(int? page);
+        Task<bool> AddCity(CityViewModel model);
+        Task<IEnumerable<CityViewModel>> DeleteCity();
+        Task<DeleteCityViewModel> DeleteConfirmed(CityViewModel model, int id);
+        Task<bool> DeleteConfirmed(int id);
+        Task<IEnumerable<CityViewModel>> Search(string searchString);
     }
 }
---
+++ FootballProjectSoftUni.Core/Services/City/CityService.cs
@@ -0,0 +1,65 @@
+﻿using FootballProjectSoftUni.Core.Contracts.City;
+using FootballProjectSoftUni.Core.Models.City;
+using FootballProjectSoftUni.Infrastructure.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using X.PagedList;
+
+
+
+namespace FootballProjectSoftUni.Core.Services.City
+{
+    public class CityService : ICityService
+    {
+        private readonly IRepository repository;
+
+        public CityService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public Task<bool> AddCity(CityViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<CityViewModel>> All(int? page)
+        {
+            var pageNumber = page ?? 1;
+            var pageSize = 8;
+
+            return await repository.AllReadOnly<Infrastructure.Data.Models.City>()
+                .Select(x => new CityViewModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    ImageUrl = x.ImageUrl
+                })
+                .ToPagedListAsync(pageNumber, pageSize);
+        }
+
+        public Task<IEnumerable<CityViewModel>> DeleteCity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteCityViewModel> DeleteConfirmed(CityViewModel model, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteConfirmed(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CityViewModel>> Search(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

c6a39b1e2496568e2e0b314be95bade23ac92ecb
VladiGanchev <vladimirganchev1>
2024-03-25 21:37:07 +0200

    Updated TournamentService and interface

--- FootballProjectSoftUni.Core/Contracts/Tournament/ITournamentService.cs
+++ FootballProjectSoftUni.Core/Contracts/Tournament/ITournamentService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FootballProjectSoftUni.Core.Models.City;
+using FootballProjectSoftUni.Core.Models.Tournament;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,14 @@ namespace FootballProjectSoftUni.Core.Contracts.Tournament
 {
     public interface ITournamentService
     {
+        Task<IEnumerable<TournamentViewModel>> GetCityTournamentsAsync(int cityId);
+        Task AddTournamentToCityAsync(AddTournamentFormViewModel model, int cityId, DateTime start, DateTime end);
+        Task<DetailsViewModel> GetTournamentDetailsAsync(int id);
+        Task EditTournamentAsync(EditViewModel model, DateTime start, DateTime end);
+        Task<bool> DeleteTournamentAsync(int id);
+        Task<IEnumerable<CityViewModel>> GetCitiesAsync();
+        Task<CityViewModel> FindCityAsync(int cityId);
+        Task<EditViewModel> FindTournamentAsync(int cityId);
+        Task<FootballProjectSoftUni.Infrastructure.Data.Models.Tournament> FindTournamentByIdAsync(int id);
     }
 }
--- FootballProjectSoftUni.Core/Services/Tournament/TournamentService.cs
+++ FootballProjectSoftUni.Core/Services/Tournament/TournamentService.cs
@@ -1,13 +1,340 @@
 ﻿using FootballProjectSoftUni.Core.Contracts.Tournament;
+using FootballProjectSoftUni.Core.Models.City;
+using FootballProjectSoftUni.Core.Models.Tournament;
+using FootballProjectSoftUni.Infrastructure.Data;
+using FootballProjectSoftUni.Infrastructure.Data.Enums;
+using FootballProjectSoftUni.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static FootballProjectSoftUni.Infrastructure.Data.Constants.DataConstants;
 
 namespace FootballProjectSoftUni.Core.Services.Tournament
 {
     public class TournamentService : ITournamentService
     {
+        private readonly ApplicationDbContext data;
+
+        public TournamentService(ApplicationDbContext _data)
+        {
+            data = _data;
+        }
+
+        public async Task<AddTournamentFormViewModel> AddTournamentToCityAsync(AddTournamentFormViewModel model, int cityId)
+        {
+
+            var cities = await data.Cities.Select(x => new CityViewModel()
+            {
+                Id = x.Id,
+                Name = x.Name
+            })
+                .ToListAsync();
+
+            model.Cities = cities;
+
+            return model;
+        }
+
+        public async Task<bool> DeleteTournamentAsync(int id)
+        {
+            var tournament = await data.Tournaments
+               .Include(t => t.TournamentCities)
+                   .ThenInclude(tc => tc.City)
+               .FirstOrDefaultAsync(t => t.Id == id);
+
+
+            if (tournament == null)
+            {
+                return false;
+            }
+
+            if (tournament.RefereeId != null)
+            {
+                var referee = await data.Referees.Where(x => x.TournamentId == id).FirstOrDefaultAsync();
+
+                if (referee == null)
+                {
+                    return false;
+                }
+
+                data.Referees.Remove(referee);
+
+                await data.SaveChangesAsync();
+
+            }
+
+            var city = tournament.TournamentCities.FirstOrDefault(x => x.TournamentId == id);
+
+            if (city == null)
+            {
+                return false;
+            }
+
+            var tournamentCity = await data.TournamentsCities.Where(x => x.TournamentId == id && x.CityId == city.CityId).FirstOrDefaultAsync();
+
+            if (tournamentCity == null)
+            {
+                return false;
+            }
+
+            data.TournamentsCities.Remove(tournamentCity);
+
+            await data.SaveChangesAsync();
+
+            var tournamentParticipants = await data.TournamentsParticipants.Where(x => x.TournamentId == id).ToListAsync();
+
+            if (tournamentParticipants.Count > 0)
+            {
+                data.TournamentsParticipants.RemoveRange(tournamentParticipants);
+
+                await data.SaveChangesAsync();
+            }
+
+            var teamsId = await data.TournamentsTeams.Where(x => x.TournamentId == id).Select(x => x.TeamId).ToListAsync();
+
+            if (teamsId.Count > 0)
+            {
+                var coaches = await data.Coaches.ToListAsync();
+
+                foreach (var coach in coaches)
+                {
+                    if (teamsId.Contains(coach.TeamId ?? 0))
+                    {
+                        coach.TeamId = null;
+                    }
+                }
+
+                await data.SaveChangesAsync();
+
+                var tournametsTeams = await data.TournamentsTeams.Where(x => x.TournamentId == id).ToListAsync();
+
+                data.TournamentsTeams.RemoveRange(tournametsTeams);
+
+                await data.SaveChangesAsync();
+
+                var playersToRemove = await data.Players.Where(p => teamsId.Contains(p.TeamId ?? 0)).ToListAsync();
+
+                data.Players.RemoveRange(playersToRemove);
+
+                await data.SaveChangesAsync();
+
+                var teamsToRemove = await data.Teams.Where(t => teamsId.Contains(t.Id)).ToListAsync();
+
+                data.Teams.RemoveRange(teamsToRemove);
+
+                await data.SaveChangesAsync();
+
+
+            }
+
+
+            data.Remove(tournament);
+
+            await data.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task EditTournamentAsync(EditViewModel model, DateTime start, DateTime end)
+        {
+            var tournament = await data.Tournaments
+         .Include(t => t.TournamentCities)
+         .FirstOrDefaultAsync(t => t.Id == model.Id);
+
+            if (tournament == null)
+            {
+                throw new ArgumentException("Invalid tournament ID");
+            }
+
+            tournament.Description = model.Description;
+            tournament.StartDate = start;
+            tournament.EndDate = end;
+            tournament.ImageUrl = model.ImageUrl;
+
+            await data.SaveChangesAsync();
+        }
+
+        public async Task<CityViewModel> FindCityAsync(int id)
+        {
+            var city = await data.Cities.FindAsync(id);
+
+            if (city == null)
+            {
+                return null;
+            }
+
+            return new CityViewModel()
+            {
+                Id = city.Id,
+                Name = city.Name,
+                ImageUrl = city.ImageUrl
+            };
+        }
+
+        public async Task<IEnumerable<CityViewModel>> GetCitiesAsync()
+        {
+            return await data.Cities.Select(x => new CityViewModel()
+            {
+                Id = x.Id,
+                Name = x.Name
+            }).ToListAsync();
+        }
+
+        public async Task<IEnumerable<TournamentViewModel>> GetCityTournamentsAsync(int id)
+        {
+
+            var city = await data.Cities.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+            if (city == null)
+            {
+                return null;
+            }
+
+            var cityTournaments = await data.TournamentsCities.Where(x => x.CityId == id).Select(x => new TournamentViewModel()
+            {
+                Id = x.TournamentId,
+                StartDate = x.Tournament.StartDate,
+                EndDate = x.Tournament.EndDate,
+                RefereeId = x.Tournament.RefereeId,
+                Status = x.Tournament.Status.ToString(),
+                NumberOfTeams = x.Tournament.NumberOfTeams,
+                ImageUrl = x.Tournament.ImageUrl,
+
+
+            }).ToListAsync();
+
+            // Update tournament status based on current date
+            foreach (var tournament in cityTournaments)
+            {
+                UpdateTournamentStatus(tournament);
+
+            }
+
+            // Save changes to update tournament status in the database
+            await data.SaveChangesAsync();
+
+            return cityTournaments;
+        }
+
+        public async Task<DetailsViewModel> GetTournamentDetailsAsync(int id)
+        {
+            var needed = await data.Tournaments
+               .Include(t => t.Referee)
+               .Include(t => t.TournamentCities)
+                   .ThenInclude(tc => tc.City)
+                .Include(t => t.TournamentTeams)
+           .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (needed == null)
+            {
+                return null;
+            }
+
+            var cityId = needed.TournamentCities.FirstOrDefault().CityId;
+
+            var model = new DetailsViewModel()
+            {
+                Id = needed.Id,
+                Description = needed.Description,
+                StartDate = needed.StartDate,
+                EndDate = needed.EndDate,
+                RefereeId = needed.RefereeId,
+                RefereeName = needed.Referee?.Name,
+                CreatedOn = needed.CreatedOn,
+                Status = needed.Status.ToString(),
+                NumberOfTeams = needed.NumberOfTeams,
+                CityId = cityId
+
+            };
+
+            return model;
+
+        }
+
+        public async Task AddTournamentToCityAsync(AddTournamentFormViewModel model, int cityId, DateTime start, DateTime end)
+        {
+            var city = await data.Cities.Where(x => x.Id == cityId).FirstOrDefaultAsync();
+
+            var tournament = new FootballProjectSoftUni.Infrastructure.Data.Models.Tournament()
+            {
+                StartDate = start,
+                EndDate = end,
+                CreatedOn = DateTime.Now,
+                Description = model.Description,
+                NumberOfTeams = 0,
+                ImageUrl = model.ImageUrl,
+                Status = FootballProjectSoftUni.Infrastructure.Data.Enums.TournamentStatus.Upcoming,
+                OrganiserId = "e18907e0-947d-4e47-894a-c627618c6de4",
+                RefereeId = null
+
+            };
+
+            data.Tournaments.Add(tournament);
+
+            await data.SaveChangesAsync();
+
+            var cityTournament = new TournamentCity()
+            {
+                CityId = cityId,
+                TournamentId = tournament.Id
+            };
+
+            data.TournamentsCities.Add(cityTournament);
+
+            await data.SaveChangesAsync();
+        }
+
+
+        private void UpdateTournamentStatus(TournamentViewModel tournament)
+        {
+            if (tournament.StartDate > DateTime.Now)
+            {
+                tournament.Status = TournamentStatus.Upcoming.ToString();
+            }
+            else if (DateTime.Now > tournament.StartDate && DateTime.Now < tournament.EndDate)
+            {
+                tournament.Status = TournamentStatus.Started.ToString();
+            }
+            else if (DateTime.Now > tournament.EndDate)
+            {
+                tournament.Status = TournamentStatus.Finished.ToString();
+            }
+        }
+
+        public async Task<EditViewModel> FindTournamentAsync(int id)
+        {
+            var tournament = await data.Tournaments
+                .Include(t => t.TournamentCities)
+                   .ThenInclude(tc => tc.City)
+                   .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (tournament == null)
+            {
+                return null;
+            }
+
+            return new EditViewModel()
+            {
+                Id = tournament.Id,
+                Description = tournament.Description,
+                EndDate = tournament.EndDate.ToString(RequiredDateTimeFormat),
+                StartDate = tournament.StartDate.ToString(RequiredDateTimeFormat),
+                ImageUrl = tournament.ImageUrl,
+                CreatedOn = tournament.CreatedOn.ToString(RequiredDateTimeFormat)
+            };
+        }
+
+        public async Task<FootballProjectSoftUni.Infrastructure.Data.Models.Tournament> FindTournamentByIdAsync(int id)
+        {
+            return await data.Tournaments
+                .Include(t => t.TournamentCities)
+                    .ThenInclude(tc => tc.City)
+                .FirstOrDefaultAsync(t => t.Id == id);
+        }
     }
 }

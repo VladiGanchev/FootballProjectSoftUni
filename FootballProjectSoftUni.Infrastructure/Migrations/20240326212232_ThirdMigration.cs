5159394326b6d8afe7876d84b1aed54064b5c034
VladiGanchev <vladimirganchev1>
2024-03-27 17:54:07 +0200

    Fixed TeamId in table Players to allow null

---
+++ FootballProjectSoftUni.Infrastructure/Migrations/20240326212232_ThirdMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FootballProjectSoftUni.Infrastructure.Migrations
+{
+    public partial class ThirdMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+               name: "TeamId",
+               table: "Players",
+               type: "int",
+               nullable: true, // Променено на true, за да позволи null стойности
+               oldClrType: typeof(int),
+               oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TeamId",
+                table: "Players",
+                type: "int",
+                nullable: false, // Връщане на nullable на false
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}

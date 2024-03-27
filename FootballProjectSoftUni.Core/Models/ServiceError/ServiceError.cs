cf711d3749b956145d64eef4c1983e47dca12499
VladiGanchev <vladimirganchev1>
2024-03-27 17:47:50 +0200

    Added Error class for handling exceptions

---
+++ FootballProjectSoftUni.Core/Models/ServiceError/ServiceError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballProjectSoftUni.Core.Models.ServiceError
+{
+    public class ServiceError
+    {
+        public string Message { get; set; } = string.Empty;
+    }
+}

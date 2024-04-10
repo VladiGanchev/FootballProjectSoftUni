2475655f238d23851f823ab5d86342290f63f22e
VladiGanchev <vladimirganchev1>
2024-04-10 11:33:44 +0300

    Added Error 404 view

--- FootballProjectSoftUni/Controllers/HomeController.cs
+++ FootballProjectSoftUni/Controllers/HomeController.cs
@@ -26,14 +26,15 @@ namespace FootballProjectSoftUni.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(int statusCode)
         {
-            if (statusCode  == 400)
-            {
-                return View("Error400");
-            }
 
             if (statusCode == 401)
             {
                 return View("Error401");
+            }
+
+            if (statusCode == 404)
+            {
+                return View("Error404");
             }
 
             return View();

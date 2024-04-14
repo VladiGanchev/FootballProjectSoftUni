f902f60c65bb59b0d362bad20f7b8cea51357038
VladiGanchev <vladimirganchev1>
2024-04-14 15:35:45 +0300

    Creating database mock

---
+++ FootballProjectSoftUni.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,25 @@
+﻿using FootballProjectSoftUni.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballProjectSoftUni.Tests.Mocks
+{
+    public static class DatabaseMock
+    {
+        public static ApplicationDbContext Instance
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase("ApplicationInMemoryDb" + DateTime.Now.Ticks.ToString())
+                    .Options;
+
+                return new ApplicationDbContext(dbContextOptions, false);
+            }
+        }
+    }
+}
